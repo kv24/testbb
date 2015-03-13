d892199bdc3065040cf95701c12aaeb1c4a2a3b3
xumx <kv24@163.com>
2015-03-13 11:05:25 +0800

    new

---
+++ src/main/java/bb/Test.java
@@ -0,0 +1,10 @@
+package bb;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(123);
+		System.out.println(4456);
+		System.out.println(567);
+		System.out.println(567333388);
+	}
+}
